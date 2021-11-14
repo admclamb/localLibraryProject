c790c9dc0a05bd0278d3f13777e99f6a95258b12
Anthony McLamb <90528159+admclamb@users.noreply.github.com>
2021-11-13 21:01:29 -0500

    Update home.js
    Adding comments to home.js

--- public/src/home.js
+++ public/src/home.js
@@ -8,6 +8,7 @@ function getTotalAccountsCount(accounts) {
 
 function getBooksBorrowedCount(books) {
   return books.reduce((accumulator, book) => {
+//     Uses the index of 0 on borrows as the first item tells if returned.
     if (!book.borrows[0].returned) accumulator++;
     return accumulator;
   }, 0);
@@ -15,6 +16,7 @@ function getBooksBorrowedCount(books) {
 
 function getMostCommonGenres(books) {
   const notepad = books.reduce((acc, book) => {
+    
     if (acc[book.genre]) {
       acc[book.genre] += 1;
     } else {
@@ -23,6 +25,7 @@ function getMostCommonGenres(books) {
     return acc;
   }, {});
   let genres = [];
+//   adding objects to the final array.
   for (let item in notepad) {
     genres.push({ name: item, count: notepad[item] });
   }
