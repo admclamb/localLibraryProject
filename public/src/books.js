ce0366e725236537b6c878b84214522defdedbdd
Anthony McLamb <90528159+admclamb@users.noreply.github.com>
2021-11-13 20:57:25 -0500

    Update books.js
    Updated more comments in book.js

--- public/src/books.js
+++ public/src/books.js
@@ -10,6 +10,7 @@ function partitionBooksByBorrowedStatus(books) {
   let booksOut = [];
   let booksIn = [];
   books.forEach((object) => {
+//     Checks an the first item of an array to see if user returned.
     let isReturned = object.borrows[0].returned;
     isReturned ? booksIn.push(object) : booksOut.push(object);
   });
@@ -19,10 +20,12 @@ function partitionBooksByBorrowedStatus(books) {
 function getBorrowersForBook(book, accounts) {
   let { borrows } = book;
   let borrowers = accounts.filter((element) => {
+//    See if id in borrows.
     return borrows.map((object) => object.id).includes(element.id);
   });
   let borrowersFormatted = borrowers.map((element) => {
     const { id } = element;
+//    Finds an object of the user based on element id
     const userFound = borrows.find((item) => item.id === id);
     element["returned"] = userFound.returned;
     return element;
